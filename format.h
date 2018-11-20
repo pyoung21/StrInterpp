b6ef2fa68c2bcd5238a9f2ef9e07ed4fd76bbe49
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2018-11-20 15:48:04 +0100

    Update format.h

--- format.h
+++ format.h
@@ -1,6 +1,8 @@
+#include <iostream>
 #include <iterator>
+#include <array>
 
-#define FMT(s) [] ()  { \
+#define FMT(s) [] () constexpr -> auto { \
 	struct _ { \
 		static constexpr decltype(auto) value() { return s; } \
 		static constexpr size_t size() { return std::size(s) - 1; } \
@@ -12,24 +14,43 @@
 namespace internal
 {
 	template<typename Fmt>
-	constexpr size_t count_args(Fmt fmt)
+	constexpr auto count_args(Fmt fmt) -> size_t
 	{
 		size_t count = 0;
-		for (size_t i = 0; i < fmt.size() - 1; i += 2)
+		for (size_t i = 0; i < fmt.size() - 1;)
 		{
 			if (fmt.at(i) == '{')
 			{
-				if (fmt.at(i + 1) == '}')
+				i++;
+				if (fmt.at(i) == '}')
+				{
 					count++;
+					i++;
+				}
 			}
+			i++;
 		}
 		return count;
+	}
+
+	template<typename T>
+	constexpr auto get_arg_size()
+	{
+		return sizeof(T);
+	}
+
+	template<typename Fmt, typename... Args>
+	constexpr auto get_format_size()
+	{
+		return (0 + ... + get_arg_size<Args>());
 	}
 }
 
 template<typename Fmt, typename... Args>
-constexpr decltype(auto) sformat(Fmt, Args&&... args)
+constexpr auto sformat(Fmt, Args&&... args)
 {
 	static_assert(internal::count_args(Fmt{}) == sizeof...(args), "fmt: Arguments don't match format.");
-	return 1;
+	std::array<char, internal::get_format_size<Fmt, Args...>()> ret{ 0 };
+	constexpr auto t = internal::get_format_size<Fmt, Args...>();
+	return ret;
 }
