6bf63fdea04b56247c54e5fdb40ad4815775b110
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2018-11-30 18:03:34 +0100

    Update format.h

--- format.h
+++ format.h
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <iterator>
+// Fmt.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <array>
 
 #define FMT(s) [] () constexpr -> auto { \
@@ -11,46 +12,106 @@
 	return _{}; \
 }()
 
-namespace internal
+auto _fmt_ = FMT("");
+using PYSTRING_Type = decltype(_fmt_);
+
+template<typename Fmt>
+constexpr auto count_args(Fmt fmt) -> size_t
 {
-	template<typename Fmt>
-	constexpr auto count_args(Fmt fmt) -> size_t
+	size_t count = 0;
+	for (size_t i = 0; i < fmt.size() - 1;)
 	{
-		size_t count = 0;
-		for (size_t i = 0; i < fmt.size() - 1;)
+		if (fmt.at(i) == '{')
 		{
-			if (fmt.at(i) == '{')
-			{
-				i++;
-				if (fmt.at(i) == '}')
-				{
-					count++;
-					i++;
-				}
-			}
 			i++;
+			if (fmt.at(i) == '}')
+			{
+				count++;
+				i++;
+			}
 		}
-		return count;
+		i++;
 	}
+	return count;
+}
 
-	template<typename T>
-	constexpr auto get_arg_size()
-	{
-		return sizeof(T);
-	}
+template<typename T>
+constexpr bool is_string(T t)
+{
+	return false;
+}
 
-	template<typename Fmt, typename... Args>
-	constexpr auto get_format_size()
-	{
-		return (0 + ... + get_arg_size<Args>());
-	}
+template<>
+constexpr bool is_string(const char*)
+{
+	return true;
+}
+
+template<size_t N>
+constexpr bool is_string(const char(&)[N])
+{
+	return true;
+}
+
+template<typename T>
+constexpr auto get_arg_size()
+{
+	if constexpr (is_string(T{}))
+		return sizeof(T) - 1;
+	return sizeof(T);
 }
 
 template<typename Fmt, typename... Args>
-constexpr auto sformat(Fmt, Args&&... args)
+constexpr auto get_format_size()
 {
-	static_assert(internal::count_args(Fmt{}) == sizeof...(args), "fmt: Arguments don't match format.");
-	std::array<char, internal::get_format_size<Fmt, Args...>()> ret{ 0 };
+	return (0 + ... + get_arg_size<Args>()) + (Fmt::size() - (sizeof...(Args) * 2));
+}
 
-	return ret;
+template<typename T>
+constexpr int formatArg(char *data, T arg, size_t total)
+{
+	for (int i = 0; i < total; i++)
+	{
+		if (data[i] == 0)
+		{
+			for (int i = 0; i < sizeof(T); i++)
+				data[i] = ((arg >> (8 * i)) & 0xff);
+		}
+	}
+	return 0;
+}
+
+template<typename Fmt>
+constexpr int writeFormat(char *data, Fmt fmt)
+{
+	for (int i = 0; i < fmt.size();)
+	{
+		if (fmt.at(i) == '{' || fmt.at(i) == '}')
+			data[i] = 0;
+		else
+			data[i] = fmt.at(i);
+		i++;
+	}
+	return 0;
+}
+
+template<typename Fmt, typename... Args>
+constexpr auto myFormat(Fmt, Args&&... args)
+{
+	static_assert(count_args(Fmt{}) == sizeof...(args), "fmt: Arguments don't match format.");
+
+	std::array<char, get_format_size<Fmt, Args...>()> data{ 0 };
+	auto *pData = data.data();
+
+	int l[] = { writeFormat(pData, Fmt{}), formatArg(pData, args, data.size())... };
+
+	return data;
+}
+
+#define $(fmt, ...) (myFormat(FMT(fmt), __VA_ARGS__))
+
+int main()
+{
+	//constexpr auto t = is_pystring<decltype(_fmt_)>::value;
+	constexpr auto str = $("my format {} {} {}", 1, 2, 3);
 }
